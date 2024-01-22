baa563ed0a37569d31575a09e75b2a7b946f1505
Ogollah <kogollah2011@gmail.com>
2024-01-22 09:54:45 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 A method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
@@ -10,20 +10,11 @@ def minOperations(n):
     Calculates the fewest number of operations needed to
     result in exactly n H characters.
     """
-    if not isinstance(n, int) or n <= 1:
+    if n <= 1:
         return 0
 
-    ops_count = 0
-    clipboard = 0
-    done = 1
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
 
-    while done < n:
-        if clipboard == 0 or (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif clipboard > 0:
-            done += clipboard
-            ops_count += 1
-
-    return ops_count
+    return n
