b7ed2e378523a3fbd92b37e9f7db92b1bba9bfa7
Ogollah <kogollah2011@gmail.com>
2024-02-24 23:33:12 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,20 @@
 #!/usr/bin/node
 
+/*a script that prints all characters of a Star Wars movie:*/
+
 const request = require('request');
 
-// Function to retrieve character names in the exact order
-const getCharacterNames = (filmId) => {
-    const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-
-    // Making a request to get film details
-    request(url, (err, res, body) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        // Parsing film details
-        const filmData = JSON.parse(body);
-        const characters = filmData.characters;
-
-        // Displaying character names in the exact order
-        characters.forEach((characterUrl) => {
-            request(characterUrl, (err, res, body) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-
-                const characterData = JSON.parse(body);
-                console.log(characterData.name);
-            });
-        });
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
 };
-
-// Handling command line argument
-const filmId = process.argv[2];
-if (!filmId) {
-    console.error('Please provide a film ID as a command line argument.');
-    process.exit(1);
-}
-
-// Calling the function to retrieve and display character names
-getCharacterNames(filmId);
